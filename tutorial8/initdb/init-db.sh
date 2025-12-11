6bdc8c433ecbe26302195b7564ac15aabc7dd05f
djamn <david@jamnig.net>
2025-12-12 00:04:26 +0100

    fix

--- tutorial8/initdb/init-db.sh
+++ tutorial8/initdb/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Führe SQL-Skripte aus, um Datenbankschemata und -daten zu initialisieren
+# Executes SQL script to initialize database schema and data
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \i /docker-entrypoint-initdb.d/create_tables.sql
 EOSQL
