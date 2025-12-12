02284615a519e7dfd3ae2c6ef65dc369f0cb37e9
djamn <david@jamnig.net>
2025-12-12 18:48:29 +0100

    fixes

--- tutorial8/frontend/src/app/app.routes.ts
+++ tutorial8/frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import {Bikes} from './pages/bikes/bikes';
 
 export const routes: Routes = [
-  {path: '', component: Bikes}
+  {path: '', component: Bikes},
+  {path: 'bike/testing', component: Bikes} // path would be localhost:3000/bike/testing
 ];
